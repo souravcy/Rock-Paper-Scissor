5a5cd15203a1af3e9f78b00e2920e9ecd14af1c6
Sourav Chakraborty <cy.sourav@gmail.com>
2022-10-21 20:03:07 +0530

    Javascript implement

--- script.js
+++ script.js
@@ -1,40 +1,53 @@
 function getComputerChoice() {
-	let choice = Math.floor(Math.random() * 3);
-	return choice;
+  let choice = Math.floor(Math.random() * 3);
+  return choice;
 }
-function getPlayerChoice() {
-	let choice = prompt("Please enter your choice (Rocks/Paper/Scissor): ");
-	choice = choice.toLowerCase();
-	if (choice == "rocks") return 0;
-	else if (choice == "paper") return 1;
-	else if (choice == "scissor") return 2;
-	else {
-		console.log("Please enter correct choice.");
-		getPlayerChoice();
-	}
+
+window.addEventListener("click", playerSelection);
+
+function playerSelection(e) {
+  if (`${e.target.className}` == "rock btn") {
+    updateScore(playRound(0, getComputerChoice()));
+  } else if (`${e.target.className}` == "paper btn") {
+    updateScore(playRound(1, getComputerChoice()));
+  } else if (`${e.target.className}` == "scissor btn")
+    updateScore(playRound(2, getComputerChoice()));
 }
+
 function playRound(playerSelection, computerSelection) {
-	if (playerSelection == computerSelection) return 0;
-	else if (
-		playerSelection - computerSelection == 1 ||
-		(playerSelection == 0 && computerSelection == 2)
-	)
-		return 1;
-	else if (
-		computerSelection - playerSelection == 1 ||
-		(computerSelection == 0 && playerSelection == 2)
-	)
-		return 2;
+  if (playerSelection == computerSelection) return 0;
+  else if (
+    playerSelection - computerSelection == 1 ||
+    (playerSelection == 0 && computerSelection == 2)
+  )
+    return 1;
+  else if (
+    computerSelection - playerSelection == 1 ||
+    (computerSelection == 0 && playerSelection == 2)
+  )
+    return 2;
 }
-function showWinner(winner) {
-	if (winner == 0) console.log(`Its a tie.`);
-	else if (winner == 1) console.log(`The winner is Player.`);
-	else if (winner == 2) console.log(`The winner is Computer.`);
+
+function updateScore(winner) {
+  if (winner == 0) console.log(`Its a tie.`);
+  else if (winner == 1) {
+    let container = document.querySelector(".playerscore");
+    container.textContent++;
+    haveWinner(container.textContent, winner);
+  } else if (winner == 2) {
+    let container = document.querySelector(".computerscore");
+    container.textContent++;
+    haveWinner(container.textContent);
+  }
 }
-function game() {
-	for (let i = 0; i < 2; i++) {
-		let winner = playRound(getPlayerChoice(), getComputerChoice());
-		showWinner(winner);
-	}
+
+function haveWinner(num, player) {
+  if (num == 5) {
+    let text = document.querySelector(".winner");
+    if (player == 1) text.textContent = "!!!Player wins!!!";
+    else text.textContent = "!!!Computer wins!!!";
+    setTimeout(() => {
+      document.location.reload();
+    }, 2000);
+  }
 }
-game();
