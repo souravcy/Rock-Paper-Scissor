c83f35bbbb2823da0952244b1fb606cfe88555ce
Sourav Chakraborty <cy.sourav@gmail.com>
2022-10-22 19:30:24 +0530

    Add new features

--- script.js
+++ script.js
@@ -1,31 +1,74 @@
+rockimage = "images/rock.svg";
+paperimage = "images/paper.svg";
+scissorimage = "images/scissor.svg";
+
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
   return choice;
 }
 
-window.addEventListener("click", playerSelection);
+function updateImage(playerChoice, computerChoice) {
+  let containerPlayer = document.querySelector("#playerimage");
+  let containerComputer = document.querySelector("#computerimage");
+  if (playerChoice == 0) containerPlayer.setAttribute("src", rockimage);
+  else if (playerChoice == 1) containerPlayer.setAttribute("src", paperimage);
+  else if (playerChoice == 2) containerPlayer.setAttribute("src", scissorimage);
+  if (computerChoice == 0) containerComputer.setAttribute("src", rockimage);
+  else if (computerChoice == 1)
+    containerComputer.setAttribute("src", paperimage);
+  else if (computerChoice == 2)
+    containerComputer.setAttribute("src", scissorimage);
+}
 
 function playerSelection(e) {
-  if (`${e.target.className}` == "rock btn") {
-    updateScore(playRound(0, getComputerChoice()));
-  } else if (`${e.target.className}` == "paper btn") {
-    updateScore(playRound(1, getComputerChoice()));
-  } else if (`${e.target.className}` == "scissor btn")
-    updateScore(playRound(2, getComputerChoice()));
+  if (`${e.target.className}` == "choiceimage rock") {
+    let playerChoice = 0;
+    let computerChoice = getComputerChoice();
+    updateImage(playerChoice, computerChoice);
+    updateScore(playRound(playerChoice, computerChoice));
+  } else if (`${e.target.className}` == "choiceimage paper") {
+    let playerChoice = 1;
+    let computerChoice = getComputerChoice();
+    updateImage(playerChoice, computerChoice);
+    updateScore(playRound(playerChoice, computerChoice));
+  } else if (`${e.target.className}` == "choiceimage scissor") {
+    let playerChoice = 2;
+    let computerChoice = getComputerChoice();
+    updateImage(playerChoice, computerChoice);
+    updateScore(playRound(playerChoice, computerChoice));
+  }
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection == computerSelection) return 0;
-  else if (
+  let containerSign=document.querySelector(".sign");
+  let containerPlayer = document.querySelector("#playerimage");
+  let containerComputer = document.querySelector("#computerimage");
+  let text = document.querySelector(".select");
+  if (playerSelection == computerSelection) {
+    containerSign.textContent="=";
+    containerPlayer.setAttribute('style', 'background:  rgb(239, 201, 14);');
+    containerComputer.setAttribute('style', 'background:  rgb(239, 201, 14);');
+    text.textContent = "Round : Tied";
+    return 0;
+  } else if (
     playerSelection - computerSelection == 1 ||
     (playerSelection == 0 && computerSelection == 2)
-  )
+  ) {
+    containerSign.textContent=">";
+    containerPlayer.setAttribute('style', 'background: rgb(30,206,85);');
+    containerComputer.setAttribute('style', 'background: rgb(236,76,76);');
+    text.textContent = "Round won by : Player";
     return 1;
-  else if (
+  } else if (
     computerSelection - playerSelection == 1 ||
     (computerSelection == 0 && playerSelection == 2)
-  )
+  ) {
+    containerSign.textContent="<";
+    containerPlayer.setAttribute('style', 'background: rgb(236,76,76);');
+    containerComputer.setAttribute('style', 'background: rgb(30,206,85);');
+    text.textContent = "Round won by : Computer";
     return 2;
+  }
 }
 
 function updateScore(winner) {
@@ -43,11 +86,14 @@ function updateScore(winner) {
 
 function haveWinner(num, player) {
   if (num == 5) {
-    let text = document.querySelector(".winner");
+    let text = document.querySelector(".select");
     if (player == 1) text.textContent = "!!!Player wins!!!";
     else text.textContent = "!!!Computer wins!!!";
+    window.removeEventListener("click",playerSelection);
     setTimeout(() => {
       document.location.reload();
     }, 2000);
   }
 }
+
+window.addEventListener("click", playerSelection);
